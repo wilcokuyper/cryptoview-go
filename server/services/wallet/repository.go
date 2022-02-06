4738e877c1660ae29314a8db7346226b40fe355e
Wilco Kuyper <wilcokuyper@hotmail.com>
2022-02-06 22:26:42 +0100

    Update logic for retrieving wallet items for a specific user

--- server/services/wallet/repository.go
+++ server/services/wallet/repository.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"database/sql"
+
 	"github.com/pkg/errors"
 )
 
@@ -14,18 +15,22 @@ func NewWalletRepository(db *sql.DB) *WalletRepository {
 }
 
 func (w *WalletRepository) WalletItemsForUserId(userId int) ([]*WalletItem, error) {
-	res, err := w.db.Query("SELECT * FROM wallet_items WHERE user_id = ?", userId)
+	q, err := w.db.Query("SELECT user_id, currency, amount FROM wallet_items WHERE user_id = ?", userId)
 	if err != nil {
 		return nil, errors.Wrapf(err, "GetWallet: unable to fetch wallet for user: %v", userId)
 	}
-	defer res.Close()
+	defer q.Close()
 
 	var items []*WalletItem
 
-	err = res.Scan(items)
-	if err != nil {
-		return nil, errors.Wrap(err, "WalletItemsForUserId: unable scan rows into WalletItem struct")
+	for q.Next() {
+		var item WalletItem
+		if err = q.Scan(&item.UserId, &item.Currency, &item.Amount); err != nil {
+			return nil, errors.Wrap(err, "WalletItemsForUserId: unable scan rows into WalletItem struct")
+		}
+		items = append(items, &item)
 	}
+
 
 	return items, nil
 }
